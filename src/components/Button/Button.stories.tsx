37bd7c0ab759eb0e384293dd9bc3c814017d7f55
Sota Hirose <za446131@gmail.com>
2022-04-24 01:20:12 +0900

    storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Button } from './Button'
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+export default {
+  title: 'Example/Button',
+  component: Button,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+} as ComponentMeta<typeof Button>
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+const arg = (Primary.args = {
+  link: { href: '/next', value: 'back' }
+})
+
+export const Secondary = Template.bind({})
+Secondary.args = {
+  link: {
+    ...arg.link,
+    value: 'start'
+  }
+}
