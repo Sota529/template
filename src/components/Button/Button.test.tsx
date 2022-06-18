15a7f4dadbac982ae3d4f84157586f7ff8331dc5
Sota Hirose <za446131@gmail.com>
2022-06-19 00:51:39 +0900

    testcaseごとのスコープに閉じてでバックしやすくする

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -9,21 +9,33 @@ jest.mock('src/utis', () => {
 const handleClickMock = handleClick as jest.MockedFunction<typeof handleClick>
 
 describe('<Button>', () => {
-  const props = {
-    link: {
-      href: './',
-      value: 'back'
-    }
-  }
   it('should render correctly', () => {
+    const props = {
+      link: {
+        href: './',
+        value: 'back'
+      }
+    }
     const { container } = render(<Button {...props} />)
     expect(container).toMatchSnapshot()
   })
   it('テキストが表示されているか', () => {
+    const props = {
+      link: {
+        href: './',
+        value: 'back'
+      }
+    }
     const { getByTestId } = render(<Button {...props} />)
     expect(getByTestId('button-wrapper')).toHaveTextContent('back')
   })
   it('イベントハンドラが呼ばれるか', () => {
+    const props = {
+      link: {
+        href: './',
+        value: 'back'
+      }
+    }
     const { getByTestId } = render(<Button {...props} />)
 
     fireEvent.click(getByTestId('button-wrapper'))
