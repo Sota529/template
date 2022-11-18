c38123e5a4851211b67077d6df7bc9a4ba1ac767
Sota Hirose <za446131@gmail.com>
2022-11-19 00:01:10 +0900

    await

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -6,7 +6,7 @@ if (typeof window === 'undefined') {
 } else {
   void (async () => {
     const { worker } = await import('./browser')
-    void worker.start({ onUnhandledRequest: 'bypass' })
+    await worker.start({ onUnhandledRequest: 'bypass' })
   })()
 }
 export {}
