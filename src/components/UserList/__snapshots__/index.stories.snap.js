30d11f92c70265e2d23efc1520c1a0c5bb25eaad
Sota Hirose <za446131@gmail.com>
2022-11-22 18:04:02 +0900

    add snapshot

--- src/components/UserList/__snapshots__/index.stories.snap.js
+++ src/components/UserList/__snapshots__/index.stories.snap.js
@@ -1,3 +1,18 @@
 // Jest Snapshot v1, https://goo.gl/fbAQLP
 
+exports[`Storyshots components/UserList Index 1`] = `
+Array [
+  <p
+    data-test-id={true}
+  >
+    uuid-1
+  </p>,
+  <p
+    data-test-id={true}
+  >
+    uuid-2
+  </p>,
+]
+`
+
 exports[`Storyshots components/UserList Template 1`] = `null`
